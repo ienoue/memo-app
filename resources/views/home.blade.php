455eae5c8f35f55be29cc2be48be2509f54e3e4b
ienoue <gyaagle+github@gmail.com>
2022-02-15 10:30:36 +0900

    基本的なレイアウト作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="card">
+        <div class="card-header">
+            右側パネル
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('create') }}">
+                @csrf
+                <div class="mb-3">
+                    <textarea class="form-control" rows="3" placeholder="テキストを入力して下さい"></textarea>
                 </div>
-            </div>
+                <button type="submit" class="btn btn-primary mb-3">新規作成</button>
+            </form>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
